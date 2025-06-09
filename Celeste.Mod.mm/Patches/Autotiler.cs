1913209246e55f7829a140a0eb3527114f0272fe
Snip <49392266+SnipUndercover@users.noreply.github.com>
2025-06-10 00:29:48 +0200

    Remove commented-out code

--- Celeste.Mod.mm/Patches/Autotiler.cs
+++ Celeste.Mod.mm/Patches/Autotiler.cs
@@ -69,17 +69,7 @@ namespace Celeste {
                 Logger.Debug("Autotiler", $"Reading template for tileset with id '{data.ID}', scan height {data.ScanHeight}, and scan width {data.ScanWidth}.");
                 ReadIntoCustomTemplate(data, tileset, xml);
             }
-
-            /*if (xml.HasAttr("soundPath") && xml.HasAttr("sound")) { // Could accommodate for no sound attr, but requiring it should improve clarity on user's end
-                SurfaceIndex.TileToIndex[xml.AttrChar("id")] = xml.AttrInt("sound");
-                patch_SurfaceIndex.IndexToCustomPath[xml.AttrInt("sound")] = (xml.Attr("soundPath").StartsWith("event:/") ? "" : "event:/") + xml.Attr("soundPath");
-            } else if (xml.HasAttr("sound")) {
-                SurfaceIndex.TileToIndex[xml.AttrChar("id")] = xml.AttrInt("sound");
-            } else if (!SurfaceIndex.TileToIndex.ContainsKey(xml.AttrChar("id"))) {
-                SurfaceIndex.TileToIndex[xml.AttrChar("id")] = 0; // fall back to no sound
-            }*/
-            // Rewritten below in order to support more sound attributes
-
+            
             if (xml.HasAttr("sound")) {
                 SurfaceIndex.TileToIndex[xml.AttrChar("id")] = xml.AttrInt("sound");
                 // Could accommodate for no sound attr, but requiring it should improve clarity on user's end
