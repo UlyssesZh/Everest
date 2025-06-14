a2a360fd3a59779c0fea97f6660932366a3519e8
Maddie <52103563+maddie480@users.noreply.github.com>
2025-06-14 20:39:24 +0200

    Revert "Merge pull request #890 from SnipUndercover/reload-vanillla-portraits-on-map-exit"
    This reverts commit 7bd41c26850bbdfef937e2ed929174e864101c4c, reversing
    changes made to 64178c9089dc2320da8d0e156f897eebdd104340.

--- Celeste.Mod.mm/Patches/Level.cs
+++ Celeste.Mod.mm/Patches/Level.cs
@@ -20,7 +20,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
@@ -590,13 +589,8 @@ namespace Celeste {
         public override void End() {
             orig_End();
 
-            // reload the vanilla Portraits.xml in case it was overridden by a map
-            // else, if the Portraits.xml doesn't have portrait_madeline defined,
-            // the game will crash when trying to load a save file (since it shows madeline's portrait)
-            GFX.PortraitsSpriteBank = new SpriteBank(GFX.Portraits, Path.Combine("Graphics", "Portraits.xml"));
-
             // if we are not entering PICO-8 or the Reflection Fall cutscene...
-            if (patch_Engine.NextScene is not (Pico8.Emulator or OverworldReflectionsFall)) {
+            if (!(patch_Engine.NextScene is Pico8.Emulator) && !(patch_Engine.NextScene is OverworldReflectionsFall)) {
                 // break all links between this level and its entities.
                 foreach (Entity entity in Entities) {
                     ((patch_Entity) entity).DissociateFromScene();
