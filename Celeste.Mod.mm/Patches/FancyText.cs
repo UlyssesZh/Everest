e47f67fc8c4b0b60b0a75112c5c90704ed371040
Maddie <52103563+maddie480@users.noreply.github.com>
2025-08-23 20:14:28 +0200

    Revert "Merge pull request #925 from maddie480/fancy-unicode-characters"
    This reverts commit 99b5252db6ae804d378c0c1e35ca4ad67096a57d, reversing
    changes made to 7bf5e7eb318c0b9877f537165754330cd3746d6d.

--- Celeste.Mod.mm/Patches/FancyText.cs
+++ Celeste.Mod.mm/Patches/FancyText.cs
@@ -3,13 +3,11 @@
 using Celeste.Mod;
 using Microsoft.Xna.Framework;
 using Monocle;
-using MonoMod;
 
 namespace Celeste {
     // The FancyText ctor is private.
     class patch_FancyText {
 
-        [PatchTextIteration]
         private extern void orig_AddWord(string word);
         private void AddWord(string word) {
             word = Emoji.Apply(word);
