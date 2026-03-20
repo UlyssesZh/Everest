bb61ee39f73591cf134a9effd0225e6007fba45b
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-20 17:16:58 +0000

    add ability to customize room colors on the debug map

--- Celeste.Mod.mm/Patches/LevelTemplate.cs
+++ Celeste.Mod.mm/Patches/LevelTemplate.cs
@@ -1,4 +1,5 @@
 ﻿using Celeste.Mod.Helpers;
+using Microsoft.Xna.Framework;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Monocle;
@@ -10,6 +11,9 @@ using System.Runtime.CompilerServices;
 
 namespace Celeste.Editor {
     class patch_LevelTemplate : LevelTemplate {
+        // expose this private field to our patch
+        private static Color[] fgTilesColor;
+        
         public patch_LevelTemplate(LevelData data)
             : base(data) {
             // no-op. MonoMod ignores this - we only need this to make the compiler shut up.
@@ -21,6 +25,7 @@ namespace Celeste.Editor {
         public extern void ctor(LevelData data);
 
         [AddLevelTemplateCulling]
+        [PatchLevelTemplateRenderContents]
         [MonoModIgnore]
         public new extern void RenderContents(Camera camera, System.Collections.Generic.List<LevelTemplate> allLevels);
 
@@ -36,6 +41,24 @@ namespace Celeste.Editor {
         private bool IsVisible(Camera camera) {
             return CullHelper.IsRectangleVisible(X, Y, Width, Height, camera: camera);
         }
+
+        private static Color GetEditorColor(int editorColorIndex) {
+            // fallback for negative indices
+            if (editorColorIndex < 0)
+                return Color.White;
+            
+            // look it up in the list of colors if we can
+            if (editorColorIndex < fgTilesColor.Length)
+                return fgTilesColor[editorColorIndex];
+            
+            // else, unpack the color from the integer
+            // should be in format 0x00RRGGBB (we must leave space for color indices, so we can't use all 32 bits with an alpha channel)
+            uint packedColor = Convert.ToUInt32(editorColorIndex - fgTilesColor.Length);
+            byte red = (byte) (packedColor >> 16 & 0xFF);
+            byte green = (byte) (packedColor >> 8 & 0xFF);
+            byte blue = (byte) (packedColor >> 0 & 0xFF);
+            return new Color(red, green, blue);
+        }
     }
 }
 
@@ -45,6 +68,12 @@ namespace MonoMod {
     /// </summary>
     [MonoModCustomMethodAttribute(nameof(MonoModRules.AddLevelTemplateCulling))]
     class AddLevelTemplateCullingAttribute : Attribute { }
+
+    /// <summary>
+    /// Patch `LevelTemplate.RenderContent` to support custom template colors.
+    /// </summary>
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchLevelTemplateRenderContents))]
+    class PatchLevelTemplateRenderContents : Attribute { }
 
     static partial class MonoModRules {
         public static void AddLevelTemplateCulling(ILContext il, CustomAttribute attrib) {
@@ -62,6 +91,37 @@ namespace MonoMod {
             cursor.Emit(OpCodes.Brtrue, label);
             cursor.Emit(OpCodes.Ret);
             cursor.MarkLabel(label);
+        }
+
+        public static void PatchLevelTemplateRenderContents(ILContext il, CustomAttribute attrib) {
+            /*
+             * patch the `fgTilesColor` lookup to use `GetEditorColor` instead
+             * ```diff
+             * - Draw.Rect(X + solid.X, Y + solid.Y, solid.Width, solid.Height, Dummy ? dummyFgTilesColor : fgTilesColor[EditorColorIndex]);
+             * + Draw.Rect(X + solid.X, Y + solid.Y, solid.Width, solid.Height, Dummy ? dummyFgTilesColor : GetEditorColor(EditorColorIndex));
+             * ```
+             */
+            
+            FieldDefinition f_LevelTemplate_EditorColorIndex = il.Method.DeclaringType.FindField("EditorColorIndex");
+            MethodDefinition m_LevelTemplate_GetEditorColor = il.Method.DeclaringType.FindMethod("GetEditorColor");
+            
+            ILCursor cursor = new(il);
+            
+            /*
+             * IL_0170: ldsfld valuetype [FNA]Microsoft.Xna.Framework.Color[] Celeste.Editor.LevelTemplate::fgTilesColor
+             * IL_0175: ldarg.0
+             * IL_0176: ldfld int32 Celeste.Editor.LevelTemplate::EditorColorIndex
+             * IL_017b: ldelem [FNA]Microsoft.Xna.Framework.Color
+             */
+            cursor.GotoNext(MoveType.Before,
+                instr => instr.MatchLdsfld("Celeste.Editor.LevelTemplate", "fgTilesColor"),
+                instr => instr.MatchLdarg0(),
+                instr => instr.MatchLdfld("Celeste.Editor.LevelTemplate", "EditorColorIndex"),
+                instr => instr.MatchLdelemAny("Microsoft.Xna.Framework.Color"));
+            cursor.RemoveRange(4);
+            cursor.EmitLdarg0();
+            cursor.EmitLdfld(f_LevelTemplate_EditorColorIndex);
+            cursor.EmitCall(m_LevelTemplate_GetEditorColor);
         }
     }
 }
